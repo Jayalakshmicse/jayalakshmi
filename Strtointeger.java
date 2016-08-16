1589125bbc9b8af8cd899be50affec2bb7300a9b
Jayalakshmicse <jayalakshmi.acse@ksrce.ac.in>
2016-08-16 17:36:39 +0530

    Create Strtointeger.java

---
+++ Strtointeger.java
@@ -0,0 +1,10 @@
+class Strtointeger
+{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+System.out.println("enter the string");
+String s1=s.next();
+int x=Integer.parseInt(s1);
+System.out.println("string to integer"+x);
+}
+}
