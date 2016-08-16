2433eeb97059907a6cc3c444271571c754e9bbff
Jayalakshmicse <jayalakshmi.acse@ksrce.ac.in>
2016-08-16 21:13:30 +0530

    Create Hash.java

---
+++ Hash.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class Hash {
+	public static void main(String[] args) {
+	String s="#";
+	String c=" ";
+	String b = " ";
+	for(int i=0;i<5;i++){
+		b+=(s+c);
+		System.out.print(b);
+	}
+	}
+
+}
